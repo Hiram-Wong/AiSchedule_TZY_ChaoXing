1e751eb7ef5f1b6c7458fd0003b4eb6d44cb18b2
HiramWong <wy-hai@qq.com>
2020-10-08 22:35:51 +0800

    1.精简代码 2.修改注释 3.规范代码

--- scheduleHtmlParser.js
+++ scheduleHtmlParser.js
@@ -1,10 +1,10 @@
 /**
- * @author  王宇海
+ * @author  Hiram
  * @date    2020-10-05
- * @version 1.1
+ * @version 1.2
  * @file    scheduleHtmlParser.js
- * @title 台州职业技术学院-台州职业技术学院综合教学管理系统-超星系统-小爱课表导入  
- * 
+ * @title 台州职业技术学院-台州职业技术学院综合教学管理系统-超星系统-小爱课表导入
+ *
  * Description①: 除函数名外都可编辑
  * Description②: 传入的参数为上一步函数获取到的html
  * Description③: 可使用正则匹配
@@ -13,12 +13,11 @@
  */
 
 
-
 /**
  * 主程序[输入课程页面的HTML字符串，提取课程信息，按约定的格式输出JSON]
  *
- * @param  String html
- * @return JSON
+ * @param html
+ * @returns {[]}
  */
 function scheduleHtmlParser(html) {
     // 加载cheerio
@@ -28,47 +27,49 @@ function scheduleHtmlParser(html) {
 
     let course = []
     let courseInfos = []
+    let time = []
 
     // 夏令时
-    const time1 = [
-        { "section": 1, "startTime": "08:00", "endTime": "08:40" },
-        { "section": 2, "startTime": "08:50", "endTime": "09:30" },
-        { "section": 3, "startTime": "09:50", "endTime": "10:30" },
-        { "section": 4, "startTime": "10:40", "endTime": "11:20" },
-        { "section": 5, "startTime": "11:30", "endTime": "12:10" },
-        { "section": 6, "startTime": "14:00", "endTime": "14:40" },
-        { "section": 7, "startTime": "14:50", "endTime": "15:30" },
-        { "section": 8, "startTime": "15:40", "endTime": "16:20" },
-        { "section": 9, "startTime": "16:30", "endTime": "17:10" },
-        { "section": 10, "startTime": "19:00", "endTime": "19:40" },
-        { "section": 11, "startTime": "19:50", "endTime": "20:30" }
+    const summerTime = [
+        {"section": 1, "startTime": "08:00", "endTime": "08:40"},
+        {"section": 2, "startTime": "08:50", "endTime": "09:30"},
+        {"section": 3, "startTime": "09:50", "endTime": "10:30"},
+        {"section": 4, "startTime": "10:40", "endTime": "11:20"},
+        {"section": 5, "startTime": "11:30", "endTime": "12:10"},
+        {"section": 6, "startTime": "14:00", "endTime": "14:40"},
+        {"section": 7, "startTime": "14:50", "endTime": "15:30"},
+        {"section": 8, "startTime": "15:40", "endTime": "16:20"},
+        {"section": 9, "startTime": "16:30", "endTime": "17:10"},
+        {"section": 10, "startTime": "19:00", "endTime": "19:40"},
+        {"section": 11, "startTime": "19:50", "endTime": "20:30"}
     ]
+
     // 冬令时
-    const time2 = [
-        { "section": 1, "startTime": "08:00", "endTime": "08:40" },
-        { "section": 2, "startTime": "08:50", "endTime": "09:30" },
-        { "section": 3, "startTime": "09:50", "endTime": "10:30" },
-        { "section": 4, "startTime": "10:40", "endTime": "11:20" },
-        { "section": 5, "startTime": "11:30", "endTime": "12:10" },
-        { "section": 6, "startTime": "13:30", "endTime": "14:10" },
-        { "section": 7, "startTime": "14:20", "endTime": "15:00" },
-        { "section": 8, "startTime": "15:10", "endTime": "15:50" },
-        { "section": 9, "startTime": "16:00", "endTime": "16:40" },
-        { "section": 10, "startTime": "18:30", "endTime": "19:10" },
-        { "section": 11, "startTime": "19:20", "endTime": "20:00" }
+    const winterTime = [
+        {"section": 1, "startTime": "08:00", "endTime": "08:40"},
+        {"section": 2, "startTime": "08:50", "endTime": "09:30"},
+        {"section": 3, "startTime": "09:50", "endTime": "10:30"},
+        {"section": 4, "startTime": "10:40", "endTime": "11:20"},
+        {"section": 5, "startTime": "11:30", "endTime": "12:10"},
+        {"section": 6, "startTime": "13:30", "endTime": "14:10"},
+        {"section": 7, "startTime": "14:20", "endTime": "15:00"},
+        {"section": 8, "startTime": "15:10", "endTime": "15:50"},
+        {"section": 9, "startTime": "16:00", "endTime": "16:40"},
+        {"section": 10, "startTime": "18:30", "endTime": "19:10"},
+        {"section": 11, "startTime": "19:20", "endTime": "20:00"}
     ]
 
     // 遍历所有有效课程单元格
     $('table[class="table-bordered"] > tbody').find('td[rowspan][class="cell"]').each(function () {
-        // 判断是否多个课程存在一个单元格内
+        // 判断是否多个课程存在一个单元格内[主要用来处理形势与政策]
         const page = $(this).find('a').length / 3
         for (let i = 0; i < page; i++) {
             // 课程
-            var name = $(this).find(`a:nth-child(${2 + i * 8})`).text()
+            const name = $(this).find(`a:nth-child(${2 + i * 8})`).text()
             // 教室
-            var position = $(this).find(`a:nth-child(${7 + i * 8})`).text()
+            const position = $(this).find(`a:nth-child(${7 + i * 8})`).text()
             // 老师
-            var teacher = $(this).find(`a:nth-child(${4 + i * 8})`).text()
+            const teacher = $(this).find(`a:nth-child(${4 + i * 8})`).text()
             // 第几周
             const weeks = $(this).children()[4 + i * 8].next.data
             const week = weeksParser(weeks)
@@ -79,8 +80,13 @@ function scheduleHtmlParser(html) {
             const sections = parseInt($(this).attr('rowspan'))
 
             // 判断冬令时或夏令时作息时间
-            dateIf ? time = time1 : time = time2
+            dateIf ? time = summerTime : time = winterTime
             course = dataParser(courseInfos, time)
+            course = {
+                courseInfos: courseInfos,
+                sectionTimes: time
+            }
+
             const section = sectionsParser(dayParser(days).startSection, sections)
             // courseInfos
             const obj = {
@@ -97,11 +103,11 @@ function scheduleHtmlParser(html) {
     console.info(course)
     return course
 
-    /** 
+    /**
      * 解析课时周数信息
      *
-     * @param  String time
-     * @return Array
+     * @param time
+     * @returns {[]}
      */
     function weeksParser(time) {
         const week = []
@@ -114,11 +120,12 @@ function scheduleHtmlParser(html) {
         return week
     }
 
-    /** 
+    /**
      * 解析课时节数信息
      *
-     * @param  String  section
-     * @return JSON
+     * @param startSection
+     * @param middle
+     * @returns {[]}
      */
     function sectionsParser(startSection, middle) {
         console.log(startSection, middle)
@@ -129,51 +136,45 @@ function scheduleHtmlParser(html) {
         return sections
     }
 
-    /** 
+    /**
      * 解析课时星期几和第几节上课信息
      *
-     * @param  String  data
-     * @return Number
+     * @param data
+     * @returns {{startDay: number, startSection: number}}
      */
     function dayParser(data) {
         const day = data.replace(/Cell/, "")
         const startDay = parseInt(day[0])
         const startSection = parseInt(day.substr(1))
-        const result = {
+        return {
             startDay: startDay,
             startSection: startSection
         }
-        return result
     }
 
-    /** 
+    /**
      * 解析课表信息
      *
-     * @param  Array  courseInfos, sectionTimes
-     * @return JSON
+     * @param courseInfos
+     * @param sectionTimes
+     * @returns {{sectionTimes: *, courseInfos: *}}
      */
     function dataParser(courseInfos, sectionTimes) {
-        const result = {
+        return {
             courseInfos: courseInfos,
             sectionTimes: sectionTimes
         }
-        return result
     }
 
-    /** 
-     * 判断是否为夏令时或冬令时
+    /**
+     * 判断作息表采用夏令时还是冬令时
      *
-     * @param
-     * @return Boolean
+     * @returns {boolean}
      */
     function dateIf() {
         const date = new Date()
         const month = date.getMonth()
         const day = date.getDate()
-        if (month >= 9 && day >= 9) {
-            return true
-        } else {
-            return false
-        }
+        return month >= 9 && day >= 9
     }
 }
--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,15 +1,26 @@
 /**
- * @author  王宇海
+ * @author  Hiram
  * @date    2020-10-05
- * @version 1.1
+ * @version 1.2
  * @file    scheduleHtmlProvider.js
- * @title 台州职业技术学院-台州职业技术学院综合教学管理系统-超星系统-小爱课表导入  
- * 
+ * @title 台州职业技术学院-台州职业技术学院综合教学管理系统-超星系统-小爱课表导入
+ *
  * Description①: 除函数名外都可编辑
  * Description②: 输入课程页面的document对象，从页面中提取课程信息的HTML片段，输出HTML字符串
  * Description③: 以下为示例，您可以完全重写或在此基础上更改
  */
 
+
+/**
+ * 解析页面获取课程表，并提供给scheduleHtmlParser.js文件处理
+ *
+ * @param iframeContent
+ * @param frameContent
+ * @param dom
+ * @returns {string}
+ */
 function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    return dom.querySelector('iframe[name=iframe7]').contentWindow.document.body.innerHTML
+    // 定义document获取课程节点
+    const documentResult = dom.querySelector('iframe[name=iframe7]')
+    return documentResult.contentWindow.document.body.innerHTML
 }
